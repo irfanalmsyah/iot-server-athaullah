69066e01e3845e6e5957b514b0694fcb8c52166b
Irfan Alamsyah <irfanalamsyah2@gmail.com>
2025-02-18 16:06:12 +0700

    feat: add primary key to feed table and include id field in Channel entity

--- internal/entities/channel.entity.go
+++ internal/entities/channel.entity.go
@@ -3,6 +3,7 @@ package entities
 import "time"
 
 type Channel struct {
+	Id     int       `json:"id"`
 	Time   time.Time `json:"time" validate:"required"`
 	Value  []float64 `json:"value" validate:"required"`
 	IdNode int       `json:"id_node" validate:"required"`
